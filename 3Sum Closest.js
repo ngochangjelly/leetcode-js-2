5f9129ee32cf633b23cf76c19fb61a0a36c7401f
Hang Tran <hang.tran@defide-ix.com>
2020-12-15 16:43:37 +0700

    .

--- 3Sum Closest.js
+++ 3Sum Closest.js
@@ -1,16 +1,3 @@
-/**
-Given an array S of n integers, find three integers in S such that the sum is closest to a given number, target. Return the sum of the three integers. You may assume that each input would have exactly one solution.
-
-    For example, given array S = {-1 2 1 -4}, and target = 1.
-
-    The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
-
-*/
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number}
- */
 var threeSumClosest = function(nums, target) {
     var len = nums.length,
         minDiff = Number.MAX_VALUE,
@@ -47,3 +34,6 @@ var threeSumClosest = function(nums, target) {
     
     return target - minDiff;
 };
+
+const res = threeSumClosest([1,2,3,4,-1,-3], 10)
+console.log('***************   ', res)
